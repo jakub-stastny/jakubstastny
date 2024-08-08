114a4cf5a7ea5c83c66acd9ce35443f36a6e7664
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-08-08 17:40:38 -0600

    WIP

--- pages/assets/components/my-footer.cljs
+++ pages/assets/components/my-footer.cljs
@@ -8,11 +8,20 @@
   (constructor [this]
                (super)
                (let [shadow (.attachShadow this #js {"mode" "open"})]
-                 (append-tag this (tag :link {:rel "stylesheet"
-                                              :href "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"}))
+                 ;; (append-tag this (tag :link {:rel "stylesheet"
+                 ;;                              :href "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"}))
+
+                 ;; (append-tag this
+                 ;;             (tag :symbol
+                 ;;                  {:id "envelope" :viewBox "0 0 512 512"}
+                 ;;                  (tag :path
+                 ;;                       {:d "M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"})))
+
+                 ;; ON READY!
                  (append-tag this (tag :footer
-                                       [(tag :p "Jakub Šťastný 2024")
+                                       [(tag :p (str "Jakub Šťastný " (.getFullYear (js/Date.))))
+                                        (tag :svg {:width 25} (tag :use {:href "#envelope"}))
                                         (no-self-referring-link
-                                         (tag :i {:class "fas fa-envelope icon"}) "/contact")])))))
+                                         (tag :i {:class "fa-solid fa-envelope"}) "/contact")])))))
 
 (js/customElements.define "my-footer" MyFooter)
