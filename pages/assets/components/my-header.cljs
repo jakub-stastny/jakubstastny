f4c4e0bf54c5843aa1d9deb5e7e9d3ebbaf9dd18
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-08-08 15:50:48 -0600

    Use CSS

--- pages/assets/components/my-header.cljs
+++ pages/assets/components/my-header.cljs
@@ -8,12 +8,7 @@
   (constructor [this]
                (super)
                (let [shadow (.attachShadow this #js {"mode" "open"})]
-                 (append-tag this
-                             (tag :style
-                                  "header { background: #f1f1f1; padding: 40px; text-align: center; position: relative; bottom: 0; width: 100%; }"))
                  (append-tag this (tag :link {:rel "stylesheet" :href "/assets/styles.css"}))
-
-                 ;; header>h1>a
                  (append-tag this (tag :header
                                        [(tag :h1 (no-self-referring-link "Jakub Šťastný" "/"))])))))
 
