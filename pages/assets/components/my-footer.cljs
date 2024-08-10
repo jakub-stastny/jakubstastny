65e86dcd176356c1417cf07650c2de0b0fec778e
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-08-10 12:29:08 -0600

    Styles

--- pages/assets/components/my-footer.cljs
+++ pages/assets/components/my-footer.cljs
@@ -9,6 +9,7 @@
                (super)
                (let [shadow (.attachShadow this #js {"mode" "open"})]
                  (append-tag this (tag :script {:type "module" :src "/assets/components/fa-icon.mjs"}))
+                 (append-tag this (tag :link {:rel "stylesheet" :href "/assets/footer.css"}))
                  (append-tag this (tag :footer
                                        [(tag :p (str "Jakub Šťastný " (.getFullYear (js/Date.))))
                                         (no-self-referring-link (tag :fa-icon {:name "envelope"}) "/contact")])))))
