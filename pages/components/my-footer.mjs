223b1f2546b354cf9c8af5d6db800d4038001038
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-08-10 16:56:09 -0600

    Automated commit from 10/08/2024 at 16:56

--- pages/components/my-footer.mjs
+++ pages/components/my-footer.mjs
@@ -5,9 +5,9 @@ class MyFooter extends HTMLElement {
 super();const self__ = this;
 const this$ = this;
 const shadow1 = this$.attachShadow(({ "mode": "open" }));
-append_tag.call(null, this$, tag.call(null, cherry_core.keyword("script"), cherry_core.array_map(cherry_core.keyword("type"), "module", cherry_core.keyword("src"), "/assets/components/fa-icon.mjs")));
-append_tag.call(null, this$, tag.call(null, cherry_core.keyword("link"), cherry_core.array_map(cherry_core.keyword("rel"), "stylesheet", cherry_core.keyword("href"), "/assets/styles.css")));
-append_tag.call(null, this$, tag.call(null, cherry_core.keyword("link"), cherry_core.array_map(cherry_core.keyword("rel"), "stylesheet", cherry_core.keyword("href"), "/assets/footer.css")));
+append_tag.call(null, this$, tag.call(null, cherry_core.keyword("script"), cherry_core.array_map(cherry_core.keyword("type"), "module", cherry_core.keyword("src"), "/components/fa-icon.mjs")));
+append_tag.call(null, this$, tag.call(null, cherry_core.keyword("link"), cherry_core.array_map(cherry_core.keyword("rel"), "stylesheet", cherry_core.keyword("href"), "/css/styles.css")));
+append_tag.call(null, this$, tag.call(null, cherry_core.keyword("link"), cherry_core.array_map(cherry_core.keyword("rel"), "stylesheet", cherry_core.keyword("href"), "/css/footer.css")));
 append_tag.call(null, this$, tag.call(null, cherry_core.keyword("footer"), cherry_core.vector(tag.call(null, cherry_core.keyword("p"), cherry_core.array_map(cherry_core.keyword("class"), "copyright"), cherry_core.str.call(null, "Jakub Šťastný ", new Date().getFullYear())), tag.call(null, cherry_core.keyword("ul"), cherry_core.array_map(cherry_core.keyword("class"), "inline-list"), cherry_core.vector(tag.call(null, cherry_core.keyword("li"), no_self_referring_link.call(null, "Main page", "/")), tag.call(null, cherry_core.keyword("li"), no_self_referring_link.call(null, "About me", "/about")), tag.call(null, cherry_core.keyword("li"), no_self_referring_link.call(null, "Contact", "/contact")))))))  }
 };
 customElements.define("my-footer", MyFooter);
--- pages/components/my-header.mjs
+++ pages/components/my-header.mjs
@@ -5,8 +5,8 @@ class MyHeader extends HTMLElement {
 super();const self__ = this;
 const this$ = this;
 const shadow1 = this$.attachShadow(({ "mode": "open" }));
-append_tag.call(null, this$, tag.call(null, cherry_core.keyword("link"), cherry_core.array_map(cherry_core.keyword("rel"), "stylesheet", cherry_core.keyword("href"), "/assets/styles.css")));
-append_tag.call(null, this$, tag.call(null, cherry_core.keyword("link"), cherry_core.array_map(cherry_core.keyword("rel"), "stylesheet", cherry_core.keyword("href"), "/assets/header.css")));
+append_tag.call(null, this$, tag.call(null, cherry_core.keyword("link"), cherry_core.array_map(cherry_core.keyword("rel"), "stylesheet", cherry_core.keyword("href"), "/css/styles.css")));
+append_tag.call(null, this$, tag.call(null, cherry_core.keyword("link"), cherry_core.array_map(cherry_core.keyword("rel"), "stylesheet", cherry_core.keyword("href"), "/css/header.css")));
 append_tag.call(null, this$, tag.call(null, cherry_core.keyword("header"), cherry_core.vector(tag.call(null, cherry_core.keyword("h1"), no_self_referring_link.call(null, "Jakub Šťastný", "/")))))  }
 };
 customElements.define("my-header", MyHeader);
