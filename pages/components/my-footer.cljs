123faf1b7c9927fe3bb5fe830904889bfb9ee4cb
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-08-11 14:37:07 -0600

    Uncopyright

--- pages/components/my-footer.cljs
+++ pages/components/my-footer.cljs
@@ -1,27 +1,35 @@
 (ns my-footer
   (:require [cherry.core :refer [defclass]]
-            [helpers :refer [append-tag tag no-self-referring-link css-var]]))
+            [helpers :refer [tag no-self-referring-link css-var]]))
+
+(defn social-icon [name link]
+  (tag :a {:href link :target "_blank" :rel "noopener"}
+       (tag :fa-icon {:name (str "brands/" name)
+                      :colour (css-var (str name "-colour"))})))
 
 (defclass MyFooter
   (extends HTMLElement)
 
   (constructor [this]
                (super)
-               (let [shadow (.attachShadow this #js {"mode" "open"})]
-                 (append-tag this (tag :script {:type "module" :src "/components/fa-icon.mjs"}))
-                 (append-tag this (tag :script {:type "module" :src "/components/my-email.mjs"}))
-                 (append-tag this (tag :link {:rel "stylesheet" :href "/css/styles.css"}))
-                 (append-tag this (tag :link {:rel "stylesheet" :href "/css/footer.css"}))
-                 (append-tag this (tag :footer
-                                       [(tag :p {:class "copyright"} (str "Jakub Šťastný " (.getFullYear (js/Date.))))
-                                        (tag :ul {:class "inline-list"}
-                                             [(tag :li
-                                                   (tag :my-email
-                                                        {:subject "Hey there!"}
-                                                        (tag :a (tag :fa-icon {:name "envelope" :colour (css-var "envelope-colour")}))))
-                                              (tag :li (tag :a {:href "https://www.youtube.com/@jakub-stastny" :target "_blank" :rel "noopener"}
-                                                            (tag :fa-icon {:name "brands/youtube" :colour (css-var "youtube-colour")})))
-                                              (tag :li (tag :a {:href "https://www.reddit.com/user/jakubstastny" :target "_blank" :rel "noopener"}
-                                                            (tag :fa-icon {:name "brands/reddit" :colour (css-var "reddit-colour")})))])])))))
+               (let [shadow-root (.attachShadow this #js {"mode" "open"})
+                     copyright
+                     (tag :p {:class "copyright"}
+                          [(tag :a {:href "/uncopyright"} "uncopyright")
+                           (str " " (.getFullYear (js/Date.)))])
+
+                     fa-envelope
+                     (tag :fa-icon {:name "envelope" :colour (css-var "envelope-colour")})
+
+                     envelope (tag :li (tag :my-email {:subject "Hey there!"} (tag :a fa-envelope)))
+                     youtube (tag :li (social-icon "youtube" "https://www.youtube.com/@jakub-stastny"))
+                     reddit  (tag :li (social-icon "reddit" "https://www.reddit.com/user/jakubstastny"))
+
+                     social-icons (tag :ul {:class "inline-list"} [envelope youtube reddit])]
+                 (.appendChild shadow-root (tag :script {:type "module" :src "/components/fa-icon.mjs"}))
+                 (.appendChild shadow-root (tag :script {:type "module" :src "/components/my-email.mjs"}))
+                 (.appendChild shadow-root (tag :link {:rel "stylesheet" :href "/css/styles.css"}))
+                 (.appendChild shadow-root (tag :link {:rel "stylesheet" :href "/css/footer.css"}))
+                 (.appendChild shadow-root (tag :footer [copyright social-icons])))))
 
 (js/customElements.define "my-footer" MyFooter)
--- pages/lib/helpers.cljs
+++ pages/lib/helpers.cljs
@@ -7,7 +7,10 @@
 
 (defn- set-children [element list]
   (doseq [child list]
-    (.appendChild element child)))
+    (.appendChild element
+                  (if (string? child)
+                    (.createTextNode js/document child)
+                    child))))
 
 (defn- set-inner-html [element html]
   (set! element -innerHTML html))
@@ -43,12 +46,6 @@
      (set-content element content)
      element)))
 
-(defn append-tag [instance tag-or-tag-name & tag-opts]
-  (let [shadow-root (.-shadowRoot instance)
-        child-node (if (symbol? tag-or-tag-name) (apply tag tag-or-tag-name tag-opts) tag-or-tag-name)]
-    ;; (js/console.log "append-tag to " shadow-root child-node)
-    (.appendChild shadow-root child-node)))
-
 (defn no-self-referring-link [title link]
   (if (= js/window.location.pathname link)
     (tag :span title)
@@ -58,3 +55,10 @@
   (let [css-var-name (str "--" name)
         computed-styles (getComputedStyle document.documentElement)]
     (.getPropertyValue computed-styles css-var-name)))
+
+;; DEPRECATED
+(defn append-tag [instance tag-or-tag-name & tag-opts]
+  (let [shadow-root (.-shadowRoot instance)
+        child-node (if (symbol? tag-or-tag-name) (apply tag tag-or-tag-name tag-opts) tag-or-tag-name)]
+    ;; (js/console.log "append-tag to " shadow-root child-node)
+    (.appendChild shadow-root child-node)))
