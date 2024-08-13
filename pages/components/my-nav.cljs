b733340b1b085a70569662e3cc3136ae7f9d9935
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-08-12 18:39:58 -0600

    Router

--- pages/components/my-nav.cljs
+++ pages/components/my-nav.cljs
@@ -1,25 +1,33 @@
 (ns my-nav
   (:require [cherry.core :refer [defclass]]
-            [helpers :refer [tag no-self-referring-link]]))
+            [helpers :refer [tag no-self-referring-link]]
+            [router :refer [router]]))
+
+(defn item-link [router-entry]
+  (no-self-referring-link (:title router-entry) (:path router-entry)))
 
 (defn render [root]
   (.appendChild root (tag :link {:rel "stylesheet" :href "/css/styles.css"}))
   (.appendChild root (tag :link {:rel "stylesheet" :href "/css/nav.css"}))
 
-  ;; Like this is quite good'n'all, but having router with all these including the titles and iterating over it would be better.
-  ;; The router would have to be ordered correctly for this, not just a map (unless the map is ordered in CLJ, I don't know).
-  (let [main (no-self-referring-link "Main page" "/")
-        about (no-self-referring-link "About me" "/about")
-        spiritual-guidance (no-self-referring-link "Spiritual guidance" "/services/spiritual-guidance")
-        astro-reading (no-self-referring-link "Intuitive astrology reading" "/services/intuitive-astrology-reading")
-        remote-healing (no-self-referring-link "Remote energy healing" "/services/remote-energy-healing")
-        services ["Services"
-                  (tag :ul [(tag :li spiritual-guidance) (tag :li astro-reading) (tag :li remote-healing)])]
-        contact (no-self-referring-link "Contact" "/contact")]
-    (.appendChild root (tag :nav (tag :ul
-                                      [(tag :li main) (tag :li about)
-                                       (tag :li services)
-                                       (tag :li contact)])))))
+  (let [main (item-link (:index router))
+        about (item-link (:about router))
+        spiritual-guidance (item-link (:spiritual-guidance router))
+        astro-reading (item-link (:intuitive-reading router))
+        remote-healing (item-link (:energy-healing router))
+        services [(item-link (:services router))
+                  (tag :ul
+                       [(tag :li spiritual-guidance)
+                        (tag :li astro-reading)
+                        (tag :li remote-healing)])]
+        contact (item-link (:contact router))]
+    (.appendChild root
+                  (tag :nav
+                       (tag :ul
+                            [(tag :li main)
+                             (tag :li about)
+                             (tag :li services)
+                             (tag :li contact)])))))
 
 (defclass MyNav
   (extends HTMLElement)
