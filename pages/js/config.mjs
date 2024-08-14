7684917e804fb7b6f927601e8c6c084573c49e93
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-08-14 15:45:11 -0600

    Pages

---
+++ pages/js/config.mjs
@@ -0,0 +1,1 @@
+import * as cherry_core from 'cherry-cljs/cljs.core.js';var email_chunks = cherry_core.vector("jakub.stastny.pt","+","web","@","gmail.com");var youtube_link = "https://www.youtube.com/@jakub-stastny";var reddit_link = "https://www.reddit.com/user/jakubstastny";export{email_chunks,youtube_link,reddit_link}
---
+++ pages/vendor/lib/node.js
@@ -0,0 +1,3 @@
+import { $APP, shadow$provide, $jscomp } from "./cljs.core.js";
+import "./compiler.js";
+const shadow_esm_import = function(x) { return import(x) };
