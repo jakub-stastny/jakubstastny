c6ec9caaa3785aa9bc12887905abd8b612bfcf9b
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-09-15 16:43:19 -0600

    Automated commit from 15/09/2024 at 16:43

--- pages/js/subscribe.mjs
+++ pages/js/subscribe.mjs
@@ -1,1 +1,1 @@
-import * as cherry_core from 'cherry-cljs/cljs.core.js';var handler =(function(event,context){return Promise.resolve(({"statusCode":200,"body":"Hello,World!"}));});export{handler}
+import * as cherry_core from 'cherry-cljs/cljs.core.js';var handler =(function(event,context){return Promise.resolve(({"statusCode":200,"body":"Hello from Cherry!"}));});export{handler}
