52eaf9181e3cb1b85fb073282979ae5349fc3621
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-09-16 12:09:16 -0600

    Automated commit from 16/09/2024 at 12:09

--- netlify/functions/subscribe.mjs
+++ netlify/functions/subscribe.mjs
@@ -27,13 +27,8 @@ const response3 = (await axios.post(endpoint, cherry_core.clj__GT_js.call(null, 
 console.log("Subscription successful:", response3.data);
 return response.call(null, 200, cherry_core.str.call(null, response.data));}
 catch(error4){
-const status5 = 400;
-const response_data6 = error4.response.data;
-console.error("Subscription failed with status code:", status5);
-console.error("Response data:", response_data6);
 console.log(error4);
-console.log(error4.response);
-return response.call(null, status5, "");}
+return response.call(null, status, "");}
 
 })());
 });
