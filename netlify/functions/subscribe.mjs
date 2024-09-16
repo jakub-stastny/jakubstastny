97fcb19152db66012f185cd5b392c6eb768871e4
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-09-16 12:17:15 -0600

    Automated commit from 16/09/2024 at 12:17

--- netlify/functions/subscribe.mjs
+++ netlify/functions/subscribe.mjs
@@ -22,11 +22,13 @@ console.log(cherry_core.str.call(null, "Subscribing ", email, "."));
 return (await (async () => {
 try{
 const data1 = cherry_core.array_map(cherry_core.keyword("email"), email, cherry_core.keyword("groups"), cherry_core.vector(group_id));
-const options2 = cherry_core.array_map(cherry_core.keyword("headers"), headers);
-const response3 = (await axios.post(endpoint, cherry_core.clj__GT_js.call(null, data1), cherry_core.clj__GT_js.call(null, options2)));
-return console.log("S", response3);}
-catch(error4){
-return console.log("E", error4);}
+const _2 = console.log("data", cherry_core.clj__GT_js.call(null, data1));
+const options3 = cherry_core.array_map(cherry_core.keyword("headers"), headers);
+const _4 = console.log("options", cherry_core.clj__GT_js.call(null, options3));
+const response5 = (await axios.post(endpoint, cherry_core.clj__GT_js.call(null, data1), cherry_core.clj__GT_js.call(null, options3)));
+return console.log("S", response5);}
+catch(error6){
+return console.log("E", error6);}
 
 })());
 });
