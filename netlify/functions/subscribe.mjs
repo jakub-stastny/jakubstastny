e24c685b7433b9f8074b0ec7402421c10444f3d2
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-09-15 19:45:56 -0600

    Automated commit from 15/09/2024 at 19:45

---
+++ netlify/functions/subscribe.mjs
@@ -0,0 +1,6 @@
+import * as cherry_core from 'cherry-cljs/cljs.core.js';
+var handler = (function (event, context) {
+return Promise.resolve(({ "statusCode": 200, "body": "Hello from Cherry!" }));
+});
+
+export { handler }
