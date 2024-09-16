fc4e22e561b4ff14ad025a6a4c6221f0453152e4
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-09-15 19:45:24 -0600

    Serverless fns

--- src/build/build_serverless.clj
+++ src/build/build_serverless.clj
@@ -6,18 +6,20 @@
             [config :as config]))
 
 (defn process-args [args]
-  (prn :pa args)
-  (fs/create-dirs config/serverless-dir)
   (doseq [source-cljs-path args]
     (let [source-mjs-path (str/replace source-cljs-path #"\.cljs$" ".mjs")
           target-path (str config/serverless-dir "/" (fs/file-name source-mjs-path))]
       (process/shell "npx" "cherry" "compile" source-cljs-path)
-      (println (str "~ JS " source-cljs-path " -> " target-path))
-      (spit target-path (slurp source-mjs-path)))))
+      (println (str "~ FN " source-cljs-path " -> " target-path))
+      (fs/move source-mjs-path target-path {:replace-existing true}))))
 
 (defn process-default []
   (fs/delete-tree config/serverless-dir)
-  (process-args
-   (remove utils/emacs-file? (map str (fs/glob config/serverless-src "*.cljs")))))
+  (fs/create-dirs config/serverless-dir)
+
+  (let [package-json (str config/serverless-src "/package.json")
+        cljs-files (map str (fs/glob config/serverless-src "*.cljs"))]
+    (fs/copy package-json (str config/serverless-dir "/package.json") {:replace-existing true})
+    (process-args (remove utils/emacs-file? cljs-files))))
 
 (utils/generate-main-fn process-args process-default)
