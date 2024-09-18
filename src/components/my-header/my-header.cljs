6a3e33bad9b6e3903bb845ad7e5d43d349068e81
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-09-18 17:00:05 -0600

    Index page & styles

--- src/components/my-header/my-header.cljs
+++ src/components/my-header/my-header.cljs
@@ -8,7 +8,7 @@
          [:link {:rel "stylesheet" :href "/css/my-header.css"}]
          [:header
           [:div {:class "wrapper" :style "opacity: 0;"}]
-          [:h1 (no-self-referring-link "Know thy Self" "/")]
-          [:h2 {:class "tagline"} "Guiding you to wholeness"]]])
+          [:h1 (no-self-referring-link "Jakub Šťastný" "/")]
+          [:h2 {:class "tagline"} "Spiritual guide & energy healer"]]])
 
 (component MyHeader "my-header" render)
