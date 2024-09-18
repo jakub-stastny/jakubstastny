363934951645d9edb2280e38e055400971ad62b3
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-09-18 07:58:47 -0600

    Subscribe

--- src/components/my-newsletter/my-newsletter.cljs
+++ src/components/my-newsletter/my-newsletter.cljs
@@ -1,10 +1,26 @@
 (ns my-newsletter
+  (:require [config :refer [custom-domain]])
   (:require-macros [macros :refer [component]]))
 
-(defn- subscribe [email]
+(defn prn [& args]
+  (apply js/console.log (map #(clj->js %) args)))
+
+(def subscribe-endpoint (str custom-domain "/.netlify/functions/subscribe"))
+
+(defn- ^:async subscribe [email]
   (js/console.log "EMAIL" email)
-  ;; TODO: POST to /.netlify/functions/subscribe
-  )
+  (let [headers
+        {:method "POST"
+         :headers {"Content-Type" "application/json"}
+         :body (js/JSON.stringify (clj->js {:email email}))}]
+    (try
+      (prn :response headers)
+      (let [response (js/await (js/fetch subscribe-endpoint (clj->js headers)))
+            _ (js/console.log response)
+            result (js/await (.json response))] ; Parse response as JSON
+        (js/console.log "Success:" result))
+      (catch js/Error e
+        (js/console.error "Error:" e)))))
 
 (defn- submit-handler [event]
   (.preventDefault event)
@@ -19,7 +35,7 @@
          [:h2 "Newsletter"]
          [:form
           [:div {:style "display: flex"}
-           [:input {:type "email" :name "email" :placeholder "you@email.com"}]
+           [:input {:type "email" :name "email" :required true :placeholder "you@email.com"}]
            [:button {:type "submit"} "Subscribe"]]]])
 
 (defn setup [component shadow-root]
