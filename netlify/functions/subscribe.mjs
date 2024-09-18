c37853f677f515248f81f43d25065ed86e43e6e8
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-09-18 08:28:01 -0600

    Automated commit from 18/09/2024 at 08:28

--- netlify/functions/subscribe.mjs
+++ netlify/functions/subscribe.mjs
@@ -24,9 +24,28 @@ return cherry_core.vector(error1, null);}
 var to_json = (function (message) {
 return cherry_core.str.call(null, JSON.stringify(({ "message": message }), null, 2), "\n");
 });
-var response = (function (status, body) {
-return ({ "statusCode": status, "body": to_json.call(null, body), "headers": ({ "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Content-Type", "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS" }) });
+var response = (() => {
+const f1 = (function (var_args) {
+const G__41 = cherry_core.alength.call(null, arguments);
+switch (G__41) {case 2:
+return f1.cljs$core$IFn$_invoke$arity$2((arguments[0]), (arguments[1]));
+break;
+case 3:
+return f1.cljs$core$IFn$_invoke$arity$3((arguments[0]), (arguments[1]), (arguments[2]));
+break;
+default:
+throw new Error(cherry_core.str.call(null, "Invalid arity: ", cherry_core.alength.call(null, arguments)))}
 });
+f1.cljs$core$IFn$_invoke$arity$2 = (function (status, body) {
+return response.call(null, status, body, cherry_core.array_map());
+});
+f1.cljs$core$IFn$_invoke$arity$3 = (function (status, body, headers) {
+const cors_headers3 = cherry_core.array_map("Access-Control-Allow-Origin", "*", "Access-Control-Allow-Headers", "Content-Type", "Access-Control-Allow-Methods", "HEAD, POST, OPTIONS");
+return ({ "statusCode": status, "body": to_json.call(null, body), "headers": cherry_core.clj__GT_js.call(null, cherry_core.merge.call(null, cors_headers3, headers)) });
+});
+f1.cljs$lang$maxFixedArity = 3;
+return f1;
+})();
 var subscribe = (async function (email) {
 console.log(cherry_core.str.call(null, "Subscribing ", email, "."));
 return (await (async () => {
@@ -43,14 +62,14 @@ return console.log(error4);}
 })());
 });
 var handle_post = (function (event, context) {
-const vec__11 = parse_json.call(null, event.body);
-const error2 = cherry_core.nth.call(null, vec__11, 0, null);
-const data3 = cherry_core.nth.call(null, vec__11, 1, null);
+const vec__51 = parse_json.call(null, event.body);
+const error2 = cherry_core.nth.call(null, vec__51, 0, null);
+const data3 = cherry_core.nth.call(null, vec__51, 1, null);
 console.log("handle-post", error2, cherry_core.clj__GT_js.call(null, data3));
 if (cherry_core.truth_.call(null, error2)) {
 return response.call(null, 400, cherry_core.ex_info.call(null, error2));} else {
 if (cherry_core.truth_.call(null, cherry_core.contains_QMARK_.call(null, data3, cherry_core.keyword("email")))) {
-return response.call(null, 200, subscribe.call(null, cherry_core.keyword("email").call(null, data3)));} else {
+return response.call(null, 201, subscribe.call(null, cherry_core.keyword("email").call(null, data3)));} else {
 if (cherry_core.truth_.call(null, cherry_core.keyword("else"))) {
 return response.call(null, 400, "Validation error: key 'email' is missing.");} else {
 return null;}}}
@@ -67,10 +86,12 @@ const method1 = event.httpMethod;
 if (cherry_core.truth_.call(null, cherry_core._EQ_.call(null, method1, "POST"))) {
 return handle_post.call(null, event, context);} else {
 if (cherry_core.truth_.call(null, cherry_core._EQ_.call(null, method1, "HEAD"))) {
-return response.call(null, 200, "");} else {
+return response.call(null, 201, "");} else {
+if (cherry_core.truth_.call(null, cherry_core._EQ_.call(null, method1, "OPTIONS"))) {
+return response.call(null, 204, "", cherry_core.array_map("Allow", "HEAD, POST, OPTIONS"));} else {
 if (cherry_core.truth_.call(null, cherry_core.keyword("else"))) {
 return response.call(null, 405, "Method not allowed");} else {
-return null;}}}
+return null;}}}}
 }));
 });
 
