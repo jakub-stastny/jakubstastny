e501a301049e2af601c371d631de3b0735f6c8ef
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-09-18 12:53:58 -0600

    Flash messages

--- src/components/my-newsletter/my-newsletter.cljs
+++ src/components/my-newsletter/my-newsletter.cljs
@@ -3,6 +3,7 @@
   (:require-macros [macros :refer [component]]))
 
 (js/import "/js/fa-icon.mjs")
+(js/import "/js/my-email.mjs")
 
 (defn prn [& args]
   (apply js/console.log (map #(clj->js %) args)))
@@ -16,23 +17,42 @@
          :body (js/JSON.stringify (clj->js {:email email}))}]
     (try
       (js/await (js/fetch subscribe-endpoint (clj->js headers)))
-        ;; TODO: Show success message.
       (catch js/Error e
-        ;; TODO: Show error message.
-        (js/console.error "Error:" e)))))
+        (js/console.error "Error:" e) false))))
+
+(def success-html
+  #html [:div "You are now subscribed to the mailing list. Thank you!"])
+
+(def error-html
+  #html [:div
+         [:h3 "Error"]
+         [:p
+          "Unfortunately it wasn't possible to subscribe to the mailing list. "
+          "Please try again and if the problem persists, "
+          "I'd really appreciate if you could let me know at "
+          [:my-email]]])
 
 (defn- generate-submit-handler [shadow-root]
   ;; Cherry doesn't support async on anonymous fns.
   (fn ^:async [event]
     (.preventDefault event)
     (let [form (.-target event)
+          flash-div (.querySelector shadow-root "#flash")
           spinner (.querySelector shadow-root "fa-icon")]
-      (prn spinner)
-      (set! (.-style.visibility spinner) "visible")
+      (set! (.-style.display spinner) "inline-block")
       ;; (js/await (subscribe (.. form -email -value)))
       (.then (subscribe (.. form -email -value))
-             (fn [_]
-               (set! (.-style.visibility spinner) "hidden"))))))
+             (fn [result]
+               (if result
+                 (do
+                   (.add (.-classList flash-div) "success")
+                   (set! (.-innerHTML flash-div) success-html)
+                   (set! (.-style.display form) "none")
+                   (set! (.-value (.-email form))))
+                 (do
+                   (.add (.-classList flash-div) "error")
+                   (set! (.-innerHTML flash-div) error-html)))
+               (set! (.-style.display spinner) "none"))))))
 
 (defn render []
   #html [:<>
@@ -40,7 +60,10 @@
          [:link {:rel "stylesheet" :href "/css/my-newsletter.css"}]
 
          [:h2 "Newsletter"]
-         [:fa-icon {:name "spinner" :style "visibility: hidden"}]
+         [:fa-icon {:name "spinner" :style "display: none"}]
+         [:div {:id "flash"}]
+         ;; [:div {:id "flash" :class "success"} success-html]
+         ;; [:div {:id "flash" :class "error"} error-html]
          [:form
           [:div {:style "display: flex"}
            [:input {:type "email" :name "email" :required true :placeholder "you@email.com"}]
